746b18ea7bfeb489723d5b288729d7092af491dc
samudralakiku <samudrala.kirankumar@gmail.com>
2026-07-01 03:28:12 -0400

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -16,7 +16,7 @@ module "eks" {
   eks_managed_node_groups = {
 
     node_group = {
-      ami_type               = "AL2023_x86_64"
+      ami_type               = "AL2023_x86_64_STANDARD"
       instance_types         = ["t3.medium"]
       vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
       min_size               = 2
